af6aefe03402f75f48df61d388ab7b92572b200e
Rafael Grimmler da Rocha <grimmlerr@gmail.com>
2023-10-07 00:42:37 -0300

    implement base map

--- src/Map.tsx
+++ src/Map.tsx
@@ -9,7 +9,7 @@ const Map: React.FC = () => {
       <MapContainer center={[-31.776, -52.3594]} zoom={13} zoomControl={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url={`https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png?api_key=${process.env.MAP_KEY}`}
+          url={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'}
         />
       </MapContainer>
     </Box>
