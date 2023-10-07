9adeece907b1dd9142b6442550cd393c34af74d3
Rafael Grimmler da Rocha <grimmlerr@gmail.com>
2023-10-07 01:03:02 -0300

    implement base map

--- src/Map.tsx
+++ src/Map.tsx
@@ -4,12 +4,14 @@ import { Box } from '@chakra-ui/react';
 import './Map.css';
 
 const Map: React.FC = () => {
+  console.log();
+
   return (
     <Box width="100vw" height="100vh">
       <MapContainer center={[-31.776, -52.3594]} zoom={13} zoomControl={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url={`https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png`}
+          url={`https://{s}.tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token=${process.env.REACT_APP_MAP_KEY}`}
         />
       </MapContainer>
     </Box>
