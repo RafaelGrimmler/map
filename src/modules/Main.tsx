e73c19e9ad5853ee699130a7aea228894a3de7ea
Rafael Grimmler da Rocha <grimmlerr@gmail.com>
2024-04-08 18:37:11 -0300

    update user map

--- src/modules/Main.tsx
+++ src/modules/Main.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 
 const Main: React.FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/map" Component={Home} />
         <Route path="/map/user/:id" Component={Home} />
         <Route path="/map/*" element={<Navigate to="/map" />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
