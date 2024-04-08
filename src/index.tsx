080b9af3ffbb5bc3c8f938cf8e401b33dc8ca9ad
Rafael Grimmler da Rocha <grimmlerr@gmail.com>
2024-04-08 19:11:46 -0300

    update user map

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/map">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>,
