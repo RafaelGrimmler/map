68959efd3f2610a98f7c2f4bb3c5f8a132823202
Rafael Grimmler <grimmlerr@gmail.com>
2024-09-25 19:56:44 -0300

    update user map

--- src/Enums.ts
+++ src/Enums.ts
@@ -1,5 +1,3 @@
 export enum NavbarEnum {LINE = "LINE", MARKER = "MARKER", USERS = "USERS"}
-
 export enum MarkerTypeEnum {DEFAULT = "DEFAULT", IMAGE = "IMAGE"}
-
 export enum UserRoleEnum {DEV = "DEV", USER = "USER"}
