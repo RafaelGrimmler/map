ac31a3f84b64c55ff0c4bf3714134ad5ba9556c2
Rafael Grimmler <rafael.rocha@bridge-mt.com>
2026-09-18 15:04:44 -0300

    change layers

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -4,14 +4,19 @@ import {
   Polyline,
   TileLayer,
   useMapEvents,
+  LayersControl,
+  Tooltip,
 } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import { User } from '../../types';
-import { StyledContainer } from './styles';
+
 import { useState } from 'react';
 import { LatLng } from 'leaflet';
+
+import { User } from '../../types';
+import { StyledContainer } from './styles';
 import Route from '../Route';
 import Line from '../Line';
+import municipios from '../../files/municipios.json';
 
 type MapProps = {
   defaultZoom?: number;
@@ -39,24 +44,35 @@ const Map: React.FC<MapProps> = ({
   const [zoom, setZoom] = useState(defaultZoom);
 
   const currentRoute = routes?.find((_, i) => selectedRoute === i);
+
   const othersRoutes = routes?.filter((_, i) => selectedRoute !== i);
+
   const currentLine = user?.lines?.find((e) => selectedLine === e?.id);
-  const othersLines = user?.lines?.filter((e) => selectedLine !== e?.id);
+
+  const othersLines = user?.lines?.filter(
+    (e) => selectedLine !== e?.id,
+  );
 
   const LocationFinderDummy = () => {
     useMapEvents({
       click: (e) => handleFindLocation?.(e?.latlng),
       zoom: (e) => setZoom(e.target._zoom),
     });
+
     return <></>;
   };
 
   const onSelectLine = (id: number) => {
-    if (!disableRoutes && !selectedLine) handleSelectLine?.(id);
+    if (!disableRoutes && !selectedLine) {
+      handleSelectLine?.(id);
+    }
   };
 
   return (
-    <StyledContainer zoom={zoom} disableRoutes={disableRoutes}>
+    <StyledContainer
+      zoom={zoom}
+      disableRoutes={disableRoutes}
+    >
       <MapContainer
         center={[-31.721742613401577, -52.35671997070313]}
         zoom={zoom}
@@ -64,20 +80,71 @@ const Map: React.FC<MapProps> = ({
         minZoom={6}
         maxZoom={16}
       >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url={`https://{s}.tile.jawg.io/jawg-streets/{z}/{x}/{y}{r}.png?access-token=${process.env.REACT_APP_MAP_KEY}`}
-        />
+        <LayersControl position="topright">
+          <LayersControl.BaseLayer
+            checked
+            name="Satélite (Esri)"
+          >
+            <TileLayer
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+              attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+            />
+          </LayersControl.BaseLayer>
+
+          <LayersControl.Overlay
+            checked
+            name="Ruas e Nomes (Esri)"
+          >
+            <TileLayer
+              url="https://tiles.stadiamaps.com/tiles/stamen_terrain_lines/{z}/{x}/{y}.png"
+              minZoom={0}
+              maxZoom={18}
+              attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+          </LayersControl.Overlay>
+        </LayersControl>
+
         <LocationFinderDummy />
+
+        {zoom >= 6 &&
+          municipios.map((municipio) => (
+            <Circle
+              key={municipio.codigo_ibge}
+              center={[
+                municipio.latitude,
+                municipio.longitude,
+              ]}
+              radius={1000}
+              pathOptions={{
+                color: '#1D51D3',
+                fillColor: '#1D51D3',
+                fillOpacity: 0.7,
+                weight: 1,
+              }}
+            >
+              <Tooltip
+                direction="top"
+                offset={[0, -5]}
+                opacity={1}
+              >
+                {municipio.nome}
+              </Tooltip>
+            </Circle>
+          ))}
+
         {othersLines?.map((e) => (
           <Polyline
             key={e?.id}
             positions={e?.lines as any}
             className="polyline"
-            eventHandlers={{ click: () => onSelectLine(e?.id) }}
+            eventHandlers={{
+              click: () => onSelectLine(e?.id),
+            }}
           />
         ))}
+
         {currentLine && <Line line={currentLine} />}
+
         {waypoints?.map((waypoint, i) => (
           <Circle
             key={`waypoint-map-${i}`}
@@ -87,10 +154,20 @@ const Map: React.FC<MapProps> = ({
             weight={3}
           />
         ))}
+
         {othersRoutes?.map((route, i) => (
-          <Route key={`route-map-${i}`} route={route} />
+          <Route
+            key={`route-map-${i}`}
+            route={route}
+          />
         ))}
-        {currentRoute && <Route route={currentRoute} selected />}
+
+        {currentRoute && (
+          <Route
+            route={currentRoute}
+            selected
+          />
+        )}
       </MapContainer>
     </StyledContainer>
   );
--- src/components/Map/styles.ts
+++ src/components/Map/styles.ts
@@ -3,7 +3,7 @@ import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 const getLineWeight = (zoom: number, constant = 1) => {
-  if (zoom <= 9) return 0.8 * constant;
+  if (zoom <= 9) return 2 * constant;
   if (zoom <= 13) return 1.3 * constant;
   if (zoom <= 14) return 1.8 * constant;
   if (zoom <= 16) return 1.4 * constant;
@@ -17,12 +17,16 @@ export const StyledContainer = styled(Box)<{
   & .leaflet-container {
     height: 100vh;
     cursor: default !important;
+
+    & .leaflet-control-layers {
+      display: none;
+    }
   }
 
   & .polyline {
     ${({ zoom, disableRoutes, theme }) => css`
       stroke-width: ${getLineWeight(zoom, 1)};
-      stroke: ${disableRoutes ? '#8a8a8a' : '#003366'};
+      stroke: ${disableRoutes ? '#8a8a8a' : '#ffcc40'};
       cursor: ${disableRoutes ? 'default' : 'pointer'};
 
       ${!disableRoutes &&
